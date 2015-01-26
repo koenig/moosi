64b572d1b11008d3293afb07476edcff9acf7d14
Stefan König <koenig.s@gmail.com>
2015-01-26 22:54:09 +0100

    currency.js

--- config/environment.js
+++ config/environment.js
@@ -50,7 +50,15 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
+    ENV.contentSecurityPolicy = {
+      'default-src': "'none'",
+      'script-src': "'self'",
+      'font-src': "'self'",
+      'connect-src': "'self'",
+      'img-src': "'self'",
+      'style-src': "'self'",
+      'media-src': "'self'"
+    }
   }
 
   return ENV;
